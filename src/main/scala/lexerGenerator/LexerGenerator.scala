2a7ad4230011c4b4ea7b84c440f3c553a3025e80
Stephen Bracken <stephen.bracken20@gmail.com>
2020-02-04 16:19:38 +0000

    added input file reading

--- src/main/scala/lexerGenerator/LexerGenerator.scala
+++ src/main/scala/lexerGenerator/LexerGenerator.scala
@@ -1,18 +1,27 @@
 package lexerGenerator
 
 import com.typesafe.scalalogging.LazyLogging
+import java.io.FileNotFoundException
+import scala.io.Source
 
 
 object Generator extends LazyLogging{
     def main(args: Array[String]): Unit = {
         try{
-        val inputFile = args(0)
-        logger.info("reading definitions from " + inputFile)
-        val outputFile = args(1)
-        logger.info("writing output to " + outputFile)
+            val currentDirectory = new java.io.File(".").getCanonicalPath
+            val inputFile = args(0)
+            logger.info("reading definitions from " + currentDirectory + '/' + inputFile)
+            val outputFile = args(1)
+            logger.info("writing output to " + outputFile)
+            val bufferedSource = Source.fromFile(inputFile)
+            for (line <- bufferedSource.getLines) {
+                println(line.toUpperCase)
+            }
+            bufferedSource.close
         }
         catch {
-            case e:Throwable => usage
+            case e:ArrayIndexOutOfBoundsException => usage
+            case e:FileNotFoundException => throw new GeneratorError(e.getMessage())
         }
     }
     def ReadRules(rules:String):Lexer = ???
@@ -20,4 +29,8 @@ object Generator extends LazyLogging{
     def usage = {
         logger.error("Usage: LexerGenerator [Definitions file] [Output filename]")
     }
+}
+
+class GeneratorError(msg: String) extends Error(msg) {
+
 }
