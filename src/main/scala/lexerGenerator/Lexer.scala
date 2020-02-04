b8144af0e6757878def0c0f8b36b4cc4078e32f2
Stephen Bracken <stephen.bracken20@gmail.com>
2020-02-04 13:58:44 +0000

    changed parameters of Lexer

--- src/main/scala/lexerGenerator/Lexer.scala
+++ src/main/scala/lexerGenerator/Lexer.scala
@@ -1,5 +1,5 @@
 package lexerGenerator
-abstract class Lexer(tokens: Token){
+abstract class Lexer(tokens: List[Token], actions: List[Action]){
     val words = tokens
     def yylex(input:String):List[Token]
 }
