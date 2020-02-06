a7dc831d0555e0af052010d6cc26b493fd0acc9f
Stephen Bracken <stephen.bracken20@gmail.com>
2020-02-06 12:25:34 +0000

    made Rule not abstract and made token public

--- src/main/scala/lexerGenerator/rules.scala
+++ src/main/scala/lexerGenerator/rules.scala
@@ -3,7 +3,7 @@ import scala.reflect.runtime.universe._
 import scala.reflect.runtime.currentMirror
 import scala.tools.reflect.ToolBox
 
-abstract class rule(r: String, private val token:Token = new NoToken, private val action:Action = new Action){
+class Rule(r: String, val token:Token = new NoToken, private val action:Action = new Action){
     private val dfa:DFA = regexParser.translateRegex(r)
     def parse(s: String) = dfa.eval(s)
     def execute:Token = {action.execute; token}
