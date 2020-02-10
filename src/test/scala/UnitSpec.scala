d074be3fe22e1bb511388ebc263c2de2055ca376
Stephen Bracken <stephen.bracken20@gmail.com>
2020-02-10 23:22:29 +0000

    updated scalatest UnitSpec

--- src/test/scala/UnitSpec.scala
+++ src/test/scala/UnitSpec.scala
@@ -1,9 +1,11 @@
 package lexerGenerator
 import org.scalatest._
+import org.scalatest.flatspec._
+import org.scalatest.matchers.should.Matchers
 import org.scalatest.concurrent.TimeLimitedTests
 import org.scalatest.time.Span
 import org.scalatest.time._
-abstract class UnitSpec extends FlatSpec with Matchers with
+abstract class UnitSpec extends AnyFlatSpec with Matchers with
   OptionValues with Inside with Inspectors with TimeLimitedTests {
     override def timeLimit: Span = Span(10000,Millis)
   }
