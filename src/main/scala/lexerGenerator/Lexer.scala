5e71ca9b30edfc80d945c686989b44dd980cd25f
Stephen Bracken <stephen.bracken20@gmail.com>
2020-02-18 11:28:19 +0000

    added I/O methods and LexerError

--- src/main/scala/lexerGenerator/Lexer.scala
+++ src/main/scala/lexerGenerator/Lexer.scala
@@ -1,7 +1,21 @@
 package lexerGenerator
-abstract class Lexer(rules: List[LexRule]){
-    def yylex(input:String):List[Token]
+class Lexer(rules: List[LexRule],private var _in:List[Char]){
+    //def yylex(input:String):List[Any]
+    private var _out:List[Char] = Nil
+    private var _c:Char = _in.head
+    //## IO methods ##
+    /** gets the next character fron the input stream */
+    def input = _c
+    /** writes a character to the output stream */
+    def output(c: Char) = {_out = c::_out}
+    /** writes a character to the input stream */
+    def unput(c: Char) = {_in = c::_in}
+    
+    //def reject
+
 }
 
-class Token(val name:String) {
+class Token(val name:String, val args:List[Any]) {
 }
+
+class LexerError(msg: String) extends Error(msg){}
