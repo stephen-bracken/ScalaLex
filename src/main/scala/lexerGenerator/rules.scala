87f71cc75c36b7b0280598fd8731a2dc5a7a22ea
Stephen Bracken <stephen.bracken20@gmail.com>
2020-02-20 12:20:07 +0000

    added start condition and list constructor

--- src/main/scala/lexerGenerator/rules.scala
+++ src/main/scala/lexerGenerator/rules.scala
@@ -4,7 +4,10 @@ import scala.reflect.runtime.currentMirror
 import scala.tools.reflect.ToolBox
 import com.typesafe.scalalogging.LazyLogging
 
-class LexRule(regex: String = "", private val action:String = "") extends LazyLogging{
+class LexRule(val startCondition: String = "", val regex: String = "",val action: String = "") extends LazyLogging{
+    def this(s: List[String]) {
+        this(s(0),s(1),s(2))
+    }
     private val dfa:DFA = regexParser.translateRegex(regex)
     def parse(s: String) = dfa(s)
     def execute = {
