538b109171e67540337e65274b4749d233c44471
Stephen-Gaming\Steve <stephen.bracken20@gmail.com>
2020-05-21 22:32:08 +0100

    updated to reflect current output

--- src/main/scala/lexerGenerator/Lexer.scala
+++ src/main/scala/lexerGenerator/Lexer.scala
@@ -3,17 +3,23 @@
 */
 //######DEFINITIONS/OUTER EXPRESSIONS/IMPORTS######
 import scala.io.Source
-import java.io.File
-import java.io.BufferedWriter
-import java.io.FileWriter
-//...user imports/companion classes...
+import java.io._
+import scalaLex.DFA
+import com.typesafe.scalalogging.LazyLogging
 
+    object Tester{
+        println("hello")
+    }
 
 class Lex {
+	//imports the state machines from the dfa file
+	private val ois = new ObjectInputStream(new FileInputStream("dfa"))
+	private val regpair:List[(DFA,String)] = ois.readObject().asInstanceOf[List[(DFA,String)]] 
+ois.close()
 	/**tracks the state of the lexer*/
 	private var state = "INITIAL"
 	/** list of valid lexing states */
-	private val states:List[String] = List("INITIAL")//list of lexing states
+	private val states:List[String] = List("INITIAL","INSTRING","INCOMMENT")
 	private val inclusive:Boolean = false
 	/**file path of input to analyse*/
 	private var _in:String = ""
@@ -23,6 +29,8 @@ class Lex {
 	private var inputseq:List[Char] = Nil
 	/**stores the next character to be processed*/
 	private var _c:Char = '\u0000'
+	private var yytext:String = ""
+	private var currMatch:String = ""
 	/**reads an input file into inputseq*/
 	private def readFile() = {
 		val bufferedSource = Source.fromFile(_in)
@@ -43,24 +51,75 @@ class Lex {
 	private def output(c: Char) = {_out = _out:+c}
 	/** writes a character to the input stream */
 	private def unput(c: Char) = {inputseq = c::inputseq}
+	/** appends the current match to the matched text*/
+	private def yymore() = {yytext += currMatch}
 	def yylex() = {
+		def f(p:List[(DFA,String)],i:Int,a:String):Unit = {
+			p match {
+				case Nil => inputseq.drop(i); doRule(a)
+				case (d,r) :: tl =>
+					val l = d.longestPrefixMatch(inputseq.asInstanceOf[String])
+					if(l._1 > i) f(tl,l._1,a)
+					else f(tl,i,a)
+			}
+		}
+		f(regpair,0,"")
 	}
 
 	//### RULES ###
 
 	/**selects the rule that matches a regex from yylex*/
 	private def doRule(r: String) = {
+		currMatch = r
+		yytext += r
 
-        //...rules...
+		//INSTRING, a*
+		def rule0() = {
+			println("AAAAAAAAAAAAAAAA");
+		}
+
+		//INITIAL, hello
+		def rule1() = {
+			println("hello");
+		}
+
+		//INITIAL, world
+		def rule2() = {
+			println("world");
+		}
+
+		//INITIAL, rule[0-9]+
+		def rule3() = {
+			println("hi");
+		}
+
+		//INITIAL, c2
+		def rule4() = {
+			
+    println("code block 2")
+
+		}
+
+		//INITIAL, add
+		def rule5() = {
+			2 + 4
+		}
 
 		//Selector
 		r match {
-            //case rule if state == ruleState => rulex()
+			case "rule[0-9]+" if state == "INITIAL" => rule3()
+			case "c2" if state == "INITIAL" => rule4()
+			case "a*" if state == "INSTRING" => rule0()
+			case "add" if state == "INITIAL" => rule5()
+			case "hello" if state == "INITIAL" => rule1()
+			case "world" if state == "INITIAL" => rule2()
 			case y => {}
 		}
 	}
 
 	//### USER SUBROUTINES ###
-	{}
+	
+println("I am in the code section");
+
 	//### END ###
 }
